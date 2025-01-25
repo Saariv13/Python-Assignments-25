82afcf30b2833ee5f8f14c413aa3a8e11d10bdb6
Saariv13 <Saariv13@gmail.com>
2025-01-24 20:08:36 -0500

    Add files via upload

---
+++ Assignment_1 Num3.py
@@ -0,0 +1,8 @@
+my_dict = input("In this format {'a': 100, 'b': 200, 'c': 300} Enter me a Dictionary:")
+if "200" in my_dict:
+    print("200 is in my_dict")
+else:
+    print("200 is not my_dict")
+
+
+
