67222f874af6932188fb3319189249bfe74b48aa
Saariv13 <Saariv13@gmail.com>
2025-01-25 12:44:23 -0500

    Add files via upload

---
+++ Assignment_2 Num2.py
@@ -0,0 +1,8 @@
+numbers = input("Enter a sequence of comma-separated numbers: ")
+
+#Formating it
+list1 = numbers.split(',')
+
+# Convert the list into a tuple
+tuple1 = tuple(list1)
+print(list1,tuple1)
